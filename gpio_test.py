df3234f10fcb25c3bc0786d3bfdc3bb6b412bb9b
Jonathan Gearhart <jonathanmav20@icloud.com>
2025-11-07 09:55:15 -0500

    Add GPIO control script for Jetson board
    This script controls a GPIO pin on a Jetson board, turning it on and off every second.

---
+++ gpio_test.py
@@ -0,0 +1,26 @@
+#example of how gpio will be controlled:
+import Jetson.GPIO as GPIO
+import time
+
+# Set the GPIO pin number mode
+GPIO.setmode(GPIO.BOARD)
+
+# Define the pin
+PIN = 29
+
+# Set up the pin as an output
+GPIO.setup(PIN, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(PIN, GPIO.HIGH)  # Turn ON
+        time.sleep(1)                # Wait 1 second
+        GPIO.output(PIN, GPIO.LOW)   # Turn OFF
+        time.sleep(1)                # Wait 1 second
+
+except KeyboardInterrupt:
+    # Clean up on CTRL+C
+    GPIO.cleanup()
+
+finally:
+    GPIO.cleanup()
